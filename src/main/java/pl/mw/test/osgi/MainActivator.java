f2fc570def69e1c52aeeb5d3bc78a27c512b9860
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2018-01-21 16:02:42 +0100

    adds slf4j dependency

--- src/main/java/pl/mw/test/osgi/MainActivator.java
+++ src/main/java/pl/mw/test/osgi/MainActivator.java
@@ -2,13 +2,17 @@ package pl.mw.test.osgi;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainActivator implements BundleActivator {
-    public void start(BundleContext context) throws Exception {
-        System.out.println("Start 1");
+    private static final Logger log = LoggerFactory.getLogger(MainActivator.class);
+
+    public void start(BundleContext context) {
+        log.info("MainActivator started.");
     }
 
-    public void stop(BundleContext context) throws Exception {
-        System.out.println("Stop 1");
+    public void stop(BundleContext context) {
+        log.info("MainActivator stopped.");
     }
 }
