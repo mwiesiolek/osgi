6fca99d749b7e8410d512783650c7f0fd0825a39
Maksymilian Wiesiolek <m.wiesiolek@globallogic.com>
2018-03-22 18:03:51 +0100

    auto updates

--- src/main/java/pl/mw/test/osgi/MainActivator.java
+++ src/main/java/pl/mw/test/osgi/MainActivator.java
@@ -1,11 +1,17 @@
 package pl.mw.test.osgi;
 
+import org.apache.felix.bundlerepository.RepositoryAdmin;
+import org.apache.felix.bundlerepository.Resolver;
+import org.apache.felix.bundlerepository.Resource;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivator implements BundleActivator {
     private static final Logger log = LoggerFactory.getLogger(MainActivator.class);
@@ -23,9 +29,29 @@ public class MainActivator implements BundleActivator {
 
         ServiceReference ref = ctx.getServiceReference(RepositoryService.class);
         RepositoryService service = (RepositoryService) ctx.getService(ref);
+        service.test();
 
-        log.info("Number of repos: {}", service.getRepos().size());
-        service.getRepos().forEach(r -> log.info(r.toString()));
+        Executors.newScheduledThreadPool(1).schedule(() -> {
+            try{
+                ServiceReference<RepositoryAdmin> repositoryAdminServiceReference = ctx.getServiceReference(RepositoryAdmin.class);
+                RepositoryAdmin repositoryAdmin = ctx.getService(repositoryAdminServiceReference);
+
+                repositoryAdmin.addRepository("http://localhost:8181/cave/http/my-repo-repository.xml");
+                Resource[] resources = repositoryAdmin.discoverResources("(&(symbolicname=osgi)(version=2.0.0-SNAPSHOT))");
+                log.info("Resource found: {}", resources.length);
+
+                Resolver resolver = repositoryAdmin.resolver();
+                resolver.add(resources[0]);
+
+                if(resolver.resolve()){
+                    // 16 = start bundle
+                    resolver.deploy(Resolver.START);
+                }
+            }catch(Exception e) {
+                log.error(e.getMessage(), e);
+            }
+
+        }, 10, TimeUnit.SECONDS);
     }
 
     public void stop(BundleContext context) {
--- src/main/java/pl/mw/test/osgi/RepositoryService.java
+++ src/main/java/pl/mw/test/osgi/RepositoryService.java
@@ -4,25 +4,17 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.repository.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component(immediate = true)
 public class RepositoryService {
+    private static final Logger log = LoggerFactory.getLogger(MainActivator.class);
 
-    private List<Repository> repos = new CopyOnWriteArrayList<>();
-
-    @Reference(cardinality = ReferenceCardinality.MULTIPLE)
-    void addRepository(Repository repo) {
-        repos.add(repo);
-    }
-
-    void removeRepository(Repository repo) {
-        repos.remove(repo);
-    }
-
-    public List<Repository> getRepos() {
-        return repos;
+    public void test() {
+        log.info("All good.");
     }
 }
